bc39e67c2cb5474f2fd75a0bde18e9bd43e5a8c7
Jamshidbek <jamshidbekd664@gmail.com>
2022-07-08 15:04:08 +0300

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import NotFind from "./components/NotFind";
+import Ferst from "./Video/Ferst"
+import Secont from "./Video/Secont"
+import There from "./Video/There"
+import Four from "./Video/Four"
+import Five from "./Video/Five"
+import Seven from "./Video/Seven"
 
+
+function App() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="ferst" element={<Ferst/>}/>
+              <Route path="about" element={<About />} />
+              <Route path="secont" element={<Secont/>}/>
+              <Route path="there" element={<There/>}/>
+              <Route path="four" element={<Four/>}/>
+              <Route path="five" element={<Five/>}/>
+              <Route path="seven" element={<Seven/>}/>
+  
+              <Route path="*" element={<NotFind />} />
+  
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
